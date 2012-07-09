8a701f8b41caaca89d54027296f6f0e4dc7a873f
Aske Hansen <aske@deeco.dk>
2012-07-09 17:23:09 +0200

    tests

---
+++ test/unit/filemanager/user_file_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+module Filemanager
+  class UserFileTest < ActiveSupport::TestCase
+    # test "the truth" do
+    #   assert true
+    # end
+  end
+end
---
+++ test/unit/helpers/filemanager/files_helper_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+
+module Filemanager
+  class FilesHelperTest < ActionView::TestCase
+  end
+end
